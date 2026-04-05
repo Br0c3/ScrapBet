499ff3c82446f0716913cf843ddbcec4483b1d5e
Br0c3 <117086321+Br0c3@users.noreply.github.com>
2026-04-05 11:32:14 +0100

    Update scrapbet.py : utilisation de fichiers excel et ajout de couleur

--- scrapbet.py
+++ scrapbet.py
@@ -3,72 +3,142 @@ from bs4 import BeautifulSoup as bfs
 import re
 from datetime import date
 from tabulate import tabulate
-import csv
-import os
+import csv, os
+import pandas as pd
 
 
 def collect_tr(indice, date=str(date.today())):
-    result = requests.get('https://www.bettingclosed.fr/predictions/date-matchs/'+date+'/bet-type/'+indice) 
-    print(result)
+    try:
+        url = f'https://www.bettingclosed.fr/predictions/date-matchs/{date}/bet-type/{indice}'
+        result = requests.get(url)
+    except:
+        print("Erreur de connexion")
+        exit()
 
     soup = bfs(result.text, 'html.parser')
-    return soup.find_all(attrs={"classe","rowincontriEven"}) + soup.find_all(attrs={"classe","rowincontriOdd"})
-   
-def parseMs(soupList):
-    r=[["Date et heure"," Region","Equipes","Resultat","Predictions"],]
+    return soup.find_all(class_="rowincontriEven") + soup.find_all(class_="rowincontriOdd")
+
+
+def parseMs(indChoice,soupList):
+    r=[["Date et heure","Region","Equipes","Resultat","Predictions","Gains ou Perte"]]
+
     for tag in soupList:
-        date = tag.find(attrs= {"class","dataMt"}).text
-        region = tag.find(attrs= {"class","iconLega"}).img["title"]
-        equipes = tag.find(attrs= {"class","matchPhone"}).get_text()
-        score = tag.find(attrs= {"class","resultMt" }).text
-        pred = tag.find(attrs={"class","predMt"})
-        prediction= pred.a.text
-        r += [[date,region,equipes,score,prediction],]
-    
+        date = tag.find(class_="dataMt")
+        region = tag.find(class_="iconLega")
+        equipes = tag.find(class_="matchPhone")
+        score = tag.find(class_="resultMt")
+        pred = tag.find(class_="predMt")
 
-        #print(date,": ",region,"| ",equipes,"|",score,prediction)
-    entete= ["Date et heure"," Region","Equipes","Resultat","Predictions"]
-    print(tabulate(r,tablefmt="rounded_grid",maxcolwidths=[None,10,10,10,10], headers=entete))
+        date = date.text if date else ""
+        try:
+          region = region.img["title"] 
+        except:
+          region = "null"
+        equipes = equipes.get_text() if equipes else ""
+        score = score.text if score else ""
+        prediction = pred.a.text if pred and pred.a else ""
+
+        r.append([date,region,equipes,score,prediction])
+        
+    (taux, predNum,pourcent,finRows) = caltaux(indChoice,r)
+    finRows += [["Taux de reussite",str(taux)+"/"+str(predNum),"En pourcentage (%)", str(pourcent)+" %",""],]
     return r
+    
+    
+def caltaux(choix,table):
+    predNum = len(table) - 1
+    taux = 0
+    for i in table:
+        flag = False
+        if ( i[0] == "Date et heure"):
+            continue
+        if (choix == "1"):
+            if (i[3].split() == i[4].split()):
+                flag = True
+            else:
+                flag = False
+        elif(choix == "3"):
+            result = i[3].split("-")
 
-def finaltouch():
-    print("\n\n**÷÷÷÷÷÷÷SCORE EXACT÷÷÷÷÷÷÷÷÷÷÷÷**")
-    yo =collect_tr("correct-scores")
-    parseMs(yo)
-    print("\n\n**++++++++++++MIXTE+++++++++++++**")
-    yo =collect_tr("mixte")
-    parseMs(yo)
-    print("**\n\n&&&&&&&&&&&&&&& Moins de but / Plus de but &&&&&&&&&&&&&&&**")
-    yo= collect_tr("under-over")
-    parseMs(yo)
-    print("**\n\n€€€€€€€€€€€€€€ BUT/ PAS BUT €€€€€€€€€€€€**")
-    yo= collect_tr("gol-nogol")
-    parseMs(yo)
+            if result[0] == '' :
+                continue
+            elif (i[4] == "Under2.5" and int(result[0])+int(result[1]) < 2.5):
+                flag = True
+            elif (i[4] == "Over2.5" and int(result[0])+int(result[1]) > 2.5):
+                flag = True
+            else:
+                flag = False
+        elif (choix == "4"):
+            if (i[4] == "Gol" and "0" not in i[3]):
+                flag =True
+            elif (i[4] == "NoGol" and "0"  in i[3]):
+                flag = True
+            else: 
+                flag = False
+        else:
+            return 0,0,0,table
 
+        if flag:
+            taux += 1
+            i.append("V")
+        else:
+            i.append("X")
+            
+    return (taux, predNum, format(taux/predNum * 100,".2f"),table)
+
+def checkDate(date):
+    
+    date_regex = re.compile(r'^\d{4}-\d{2}-\d{2}$')
+    if date_regex.match(date):
+        return True
+    else:
+        return False
+
+
+def colorset(val):
+    return "background-color: green" if val =="V" else ""
 
 def menu():
     while True:
-        dictIndice = {"1":"correct-scores","2":"mixte","3":"under-over","4":"gol-nogol"}
+        dictIndice = {"1":"correct-scores","2":"mixte","3":"under-over","4":"gol-nogol","5":"All"}
         indChoice= input(""" Choisissez l'indice voulu:\n
             1: Score exact
             2: Mixte
             3: Under-Over
             4: But-pas But
-            5: Fermer le programme
+            5: Toutes les options
+            6: Fermer le programme
         >>""")
-        if indChoice == "5":
+        if indChoice == "6":
             exit()
         dateChoice = input("Entrez la Date en suivant le format année-mois-jour: ")
+        if not checkDate(dateChoice):
+            print("Erreur de format de date : Veillez entrer la date au format année-mois-jour")
+            continue
+        if indChoice == "5" :
+          result = list()
+          for i in range(1,4):
+            result += collect_tr(indice=dictIndice[str(i)],date=    dateChoice)
+            finRows = parseMs(str(i),result)
+        else:  
+          try:
+              result = collect_tr(indice=dictIndice[indChoice],date=dateChoice)
+              finRows = parseMs(indChoice,result)
+          except:
+              print("Erreur de choix des options: Veillez choisir parmie les options proposé")
+              continue
         
-        result = collect_tr(indice=dictIndice[indChoice],date=dateChoice)
-        finRows = parseMs(result)
-        dossier = "storage/shared/BackUp-Scrabet"
+        
+        print(finRows)
+        entete= ["Date et heure"," Region","Equipes","Resultat","Predi-ctions","Gains ou Perte"]
+        print(tabulate(finRows,tablefmt="rounded_grid",maxcolwidths=[10,10,10,8,8,10], headers=entete))
+        
+        fdata = pd.DataFrame(finRows[1:], columns=finRows[0])
+
+        dossier = "~/storage/shared/BackUp-Scrabet"
         if not os.path.exists(dossier):
             os.makedirs(dossier)
-
-        with open(dossier+"/"+dictIndice[indChoice]+dateChoice+".csv", "w",encoding="utf-8") as f:
-            writer = csv.writer(f)
-            writer.writerows(finRows)
-
+        styled = fdata.style.map(colorset)
+        styled.to_excel(dossier+"/"+dictIndice[indChoice]+dateChoice+".xlsx", engine="openpyxl",index=False)
 menu()
 
